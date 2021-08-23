1db73e415ec289638d21cfff3a7e0037c66250b2
nuriamichel <nuria.l.michel.l@gmail.com>
2021-08-23 18:29:35 -0400

    monto total

--- src/app/wallet/wallet.component.ts
+++ src/app/wallet/wallet.component.ts
@@ -3,7 +3,7 @@ import { Component, Input, Output, EventEmitter } from "@angular/core";
 @Component({
   selector: "wallet",
   template: `
-  <div 
+  <div [style.background]="(eth+btc)>10 ? 'red' : 'gray'"
        style="float: left; margin: 10px; padding: 10px;">
 
 	  <p>Wallet: {{wallet}}</p>

f1b64ca950bf3c333535495ea68aad397a97f58d
nuriamichel <nuria.l.michel.l@gmail.com>
2021-08-23 03:37:16 -0400

    08/12/2021 Observer Of

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {of} from 'rxjs';
+import { filter, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -11,6 +13,31 @@ export class AppComponent {
   sw = true;
 
   color:string;
+  tictock = of([1,2,3,4,5]);
+  constructor(){
+
+    // PERSON A
+    this.tictock.pipe(
+      map(s => s.join('-')),
+      map(s => s + 'hola'),
+    ).subscribe(v => {
+      console.log('PERSON A VIDEO', v);
+    });
+    // PERSON B
+    this.tictock.pipe(
+      filter((v:any) => v[0]%2 === 1)
+    ).subscribe(v => {
+      console.log('PERSON B VIDEO', v);
+    });
+    // PERSON C
+    this.tictock.subscribe(v => {
+      console.log('PERSON C VIDEO', v);
+    });
+
+  }
+  onAddVideo(){
+
+  }
 
   printDataNuriaComp(event: any) {
     console.log('NURIA COMP:', event);

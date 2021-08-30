4ebb7c851936a517c47dda1d0009e4b428d13993
Nuria Luisa Michel Limachi <nuria.l.michel.l@gmail.com>
2021-08-30 18:20:48 -0400

    26/08/2021 sending params in navigation, listening params of navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
 import { BehaviorSubject, of, Subscription } from 'rxjs';
 import { filter, map, delay } from 'rxjs/operators';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-root',
@@ -8,10 +9,11 @@ import { filter, map, delay } from 'rxjs/operators';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  id:number = 3;
 
   data = [1,2,3,4,5,6,7,8,9];
   
-  constructor() {
+  constructor(private router: Router) {
     this.pure(2, 3);
     this.pure(10, 2);
     this.pure(5, 5);
@@ -27,5 +29,8 @@ export class AppComponent {
     const aux = Math.random();
     console.log(a + b + aux);
     return a + b + aux;
+  }
+  onGoView2FromTS():void{
+    this.router.navigate(['view2', this.id, 'sub', 33333])
   }
 }
--- src/app/view2/view2.component.ts
+++ src/app/view2/view2.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-view2',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class View2Component implements OnInit {
 
-  constructor() { }
+  constructor(private router: ActivatedRoute) {
 
-  ngOnInit() {
+  }
+
+  ngOnInit() :void {
+    /*this.router.params.subscribe(p => {
+      console.log('PARAMS', p);
+    });*/
+
+    console.log('SNAPSHOT', this.router.snapshot.params)
   }
 
 }
